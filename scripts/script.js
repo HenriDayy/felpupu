3e690dd4f289e86fbbd56b5cb2d2a73c050b1098
HenriDayy <riqincerti@gmail.com>
2026-04-12 17:33:35 -0300

    add - sou picaaa

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+document.getElementById("musica").addEventListener("click", function() {
+    var audio = new Audio("assets/Surprise.mp3");
+    audio.play(URL("assets/hoobastank"));
+    audio.play("assets/Surprise.mp3");
+    audio.play("assets/pitty.mp3");
+});
